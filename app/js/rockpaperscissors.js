cbe5ba161258595f8133d65cac3217dd41818a02
hackedbaka <jeffrey.wu23@gmail.com>
2015-04-22 10:20:24 -0700

    Revert "Unit 3 Homework"
    This reverts commit d8e92bbcbd19d49754ec254076a2565ceaa00503.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return player;
+    return /* Your Expression */;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return comp;
+    return /* Your Expression */;
 }
 
 function getWinner(playerMove,computerMove) {
