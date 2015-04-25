8754fedfb91dffcf1d91503852536c242c288809
hackedbaka <jeffrey.wu23@gmail.com>
2015-04-25 12:59:38 -0700

    working program

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
-'use strict';
+
 //function that gets an input from user from prompt
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
@@ -29,7 +29,7 @@ function getPlayerMove() {
     }
     return choice;
 }
-//function that compares the outputs from playerMove and computerMove to determine winner
+//function that compares playerMove and computerMove to determine winner
 function getWinner(playerMove,computerMove){
     var winner;
 
@@ -66,7 +66,6 @@ function getWinner(playerMove,computerMove){
 }
 
 //function that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-
 function playToFive() {
 
     console.log("Let's play Rock, Paper, Scissors");
@@ -97,3 +96,56 @@ function playToFive() {
     }
     return [playerWins, computerWins];
 }
+
+//challenge homework to play up to x amount of times
+
+//function that allows user to enter a number
+function playUp() {
+
+    console.log("Please choose a number to play to.")
+    return prompt();
+}
+
+//function to repeat the entry of a number to only allow numbers
+function getNum() { 
+
+    var num = playUp();
+    
+    while(!Number(num)) {
+        num = playUp();
+    }
+    return num;
+}
+
+//function to play to desired win amount
+function playTo() {
+
+    console.log("Let's play Rock, Paper, Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
+    var x=getNum();
+    while(playerWins < x && computerWins < x)
+    {
+        var me = getPlayerMove();
+        var comp = getComputerMove();
+        var win = getWinner(me, comp);
+        console.log("Player uses " + me + ": Computer uses " + comp);
+        if(win === "player")
+        {
+            playerWins += 1;
+            console.log("Player wins!");
+        }
+        else if(win === "computer")
+        {
+            computerWins += 1;
+            console.log("Computer wins!"); 
+        }
+        else
+            console.log("It was a tie!"); 
+        
+        console.log("Score is Player: " + playerWins + " - Computer: " + computerWins);
+        
+    }
+    return [playerWins, computerWins];
+}
+
